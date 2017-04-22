ab8a83c5c65f0f23c45fea15702892dd7d2b1350
Alin Popa <alin.popa@gmail.com>
2017-04-22 13:38:16 +0100

    Make the state monad a bit more helpful/relevant.

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,18 @@
 open Ocamlnds_ofc
+open Core.Std
 
 let run_reader () =
   let open Reader in
   let john = Person ("Johndoe", 101) in
   print_endline ((run show) john)
 
+let string_of_list xs =
+  "[" ^ (String.concat ~sep:";" xs) ^ "]"
+
 let run_state () =
   let open State in
-  let (msg, state) = ((run show) "adit") in
-  print_endline ("(" ^ msg ^ ", " ^ state ^ ")")
+  let (msg, state) = ((run_state show) []) in
+  print_endline ("(" ^ msg ^ ", " ^ (string_of_list state) ^ ")")
 
 let () =
   run_state ();
--- src/state.ml
+++ src/state.ml
@@ -1,27 +1,38 @@
 type ('s, 'a) state = State of ('s -> 'a * 's)
 
-let return a =
-  State (fun s -> (a, s))
-
-let (>>=) s f =
-  match s with
-  | State state ->
-      State (fun s ->
-        let (x, s) = state s in
-        match (f x) with
-        | State state -> state s)
-
-let run s x =
+let run_state s x =
   match s with
   | State f -> f x
 
-let get_state =
-  State (fun s -> (s, s))
+let return a =
+  State (fun s -> (a, s))
 
-let set_state s =
-  State (fun _ -> ((), s))
+let (>>=) m k =
+  State (fun s ->
+    let (a, s') = run_state m s in
+    run_state (k a) s')
+
+let push x =
+  State (fun xs ->
+    ((), x :: xs))
+
+let pop =
+  State (fun xs ->
+    match xs with
+    | x :: xs -> (Some x, xs)
+    | [] -> (None, xs))
+
+let string_of_opt x =
+  match x with
+  | Some x -> x
+  | None -> ""
 
 let show =
-  get_state >>= fun name ->
-  (set_state "tintin") >>= fun _ ->
-  return ("hello, " ^ name ^ "!")
+  push "John" >>= fun _ ->
+  pop >>= fun firstname ->
+  push "Doe" >>= fun _ ->
+  push "X" >>= fun _ ->
+  push "Y" >>= fun _ ->
+  push "Z" >>= fun _ ->
+  pop >>= fun surname ->
+  return ("hello: " ^ (string_of_opt firstname) ^ ", " ^ (string_of_opt surname) ^ "!")
